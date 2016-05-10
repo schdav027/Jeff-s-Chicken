213bbcdec7b21430c3eb3f02093d6252257ff953
David <schdav028@gmail.com>
2016-05-10 14:55:27 +0100

    Script.js2

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var greet = function () {
-    var greeting = "Hello ";
-    var name = prompt("What's your name?");
-    var greeting_message = greeting + name;
-    alert(greeting_message);
-}
-//Gets the user's name and greets them
+var greet = function (event_details) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var greeting_message = "Thank you for signing up, " + name + ". We will now bombard " + email + " with spam.";
+
+    event_details.target.style.display = "none";
+
+    var container = document.getElementById("signupformcontainer");
+    container.innerHTML = "<p>" + greeting_message + "</p>";
+
+    var welcome = document.getElementById("welcome");
+    welcome.style.fontFamily = "Comic Sans MS";
+    welcome.style.fontSize = "200%";
+    welcome.style.color = "blue";
+
+    event_details.preventDefault();
+};
+
+
+var form = document.getElementById("signupform");
+form.addEventListener("submit", greet);
+
+
