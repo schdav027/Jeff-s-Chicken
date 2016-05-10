b6f4af363bc18e290f84a9f391d85565abb31ac9
David <schdav028@gmail.com>
2016-05-10 10:59:56 +0100

    Script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var greet = function () {
+    var greeting = "Hello ";
+    var name = prompt("What's your name?");
+    var greeting_message = greeting + name;
+    alert(greeting_message);
+}
+//Gets the user's name and greets them
